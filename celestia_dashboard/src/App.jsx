4a129b79922c58adc8ebe9ec76db2829c98cc55e
rogeriomonea-del <rogeriomonea@gmail.com>
2025-08-03 02:52:38 -0300

    Add files via upload

---
+++ celestia_dashboard/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div>
+      <h1>Celes.ia Dashboard</h1>
+      <p>Bem-vindo ao painel interativo.</p>
+    </div>
+  )
+}
+
+export default App
